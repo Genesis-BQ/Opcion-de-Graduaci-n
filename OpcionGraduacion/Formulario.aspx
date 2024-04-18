198d0ad68a0aef8ce18a3e6c8c34b6a10885975b
Goagsof <gagohm@gmail.com>
2024-04-18 16:37:23 -0600

    Formulario de registro

--- OpcionGraduacion/Formulario.aspx
+++ OpcionGraduacion/Formulario.aspx
@@ -1,16 +1,46 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Formulario.aspx.cs" Inherits="OpcionGraduacion.Formulario" %>
 
 <!DOCTYPE html>
-
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title></title>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de Opción de Graduación</title>
+    <link href="CSS/formulario.css" rel="stylesheet" />
 </head>
 <body>
-    <form id="form1" runat="server">
-        <div>
-        </div>
-    </form>
+    <div class="container">
+        <h1>Registro de Opción de Graduación</h1>
+        <form id="formRegistro" runat="server">
+            <label for="email">Correo electrónico:</label>
+            <asp:TextBox ID="email" runat="server" TextMode="Email" ></asp:TextBox><br>
+
+            <label for="identificacion">Identificación:</label>
+            <asp:TextBox ID="identificacion" runat="server" ></asp:TextBox><br>
+
+            <label for="nombre">Nombre:</label>
+            <asp:TextBox ID="nombre" runat="server" ></asp:TextBox><br>
+
+            <label for="apellidos">Apellidos:</label>
+            <asp:TextBox ID="apellidos" runat="server" ></asp:TextBox><br>
+
+            <label for="carrera">Carrera:</label>
+            <asp:DropDownList ID="ddlCarreras" runat="server">
+                <asp:ListItem Text="Seleccione una carrera" Value="" />
+            </asp:DropDownList><br>
+
+            <label for="cursos_completos">Cursos Completos:</label>
+            <asp:CheckBox ID="cursos_completos" runat="server" /><br>
+
+            <label for="opcion_graduacion">Opción de Graduación:</label>
+            <asp:DropDownList ID="ddlOpcionesGraduacion" runat="server">
+                <asp:ListItem Text="Seleccione una opción" Value="" />
+            </asp:DropDownList><br>
+
+            <asp:Button ID="btnRegistrar" runat="server" Text="Registrarse" OnClick="btnRegistrar_Click" />
+            <asp:Label ID="lblMensaje" runat="server" Text=""></asp:Label>
+        </form>
+    </div>
 </body>
 </html>
+
--- OpcionGraduacion/Formulario.aspx.cs
+++ OpcionGraduacion/Formulario.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +14,89 @@ namespace OpcionGraduacion
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                CargarDatos();
+            }
+        }
 
+        private void CargarDatos()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("ObtenerDatosCarrerasOpcionesGraduacion", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        string tipo = reader["Tipo"].ToString();
+                        int id = Convert.ToInt32(reader["ID"]);
+                        string nombre = reader["Nombre"].ToString();
+                        string detalle = reader["Detalle"].ToString();
+
+                        if (tipo == "Carrera")
+                        {
+                            ddlCarreras.Items.Add(new ListItem(nombre + " - " + detalle, id.ToString()));
+                        }
+                        else if (tipo == "Opción de Graduación")
+                        {
+                            ddlOpcionesGraduacion.Items.Add(new ListItem(nombre + " - Carrera: " + detalle, id.ToString()));
+                        }
+                    }
+                    reader.Close();
+                }
+            }
+        }
+
+        protected void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string correoElectronico = email.Text;
+                int identificacionEstudiante;
+                if (!int.TryParse(identificacion.Text, out identificacionEstudiante))
+                {
+                    lblMensaje.Text = "La identificación del estudiante debe ser un número entero válido.";
+                    return;
+                }
+                string nombreEstudiante = nombre.Text;
+                string apellidoEstudiante = apellidos.Text;
+                int carreraID = Convert.ToInt32(ddlCarreras.SelectedValue);
+                bool cursosCompletos = cursos_completos.Checked;
+                int opcionGraduacionID = Convert.ToInt32(ddlOpcionesGraduacion.SelectedValue);
+
+                InsertarFormulario(correoElectronico, identificacionEstudiante, nombreEstudiante, apellidoEstudiante, carreraID, cursosCompletos, opcionGraduacionID);
+
+                Response.Redirect("Confirmacion.aspx");
+            }
+            catch (Exception ex)
+            {
+                lblMensaje.Text = "Error al registrar el formulario: " + ex.Message;
+            }
+        }
+
+        private void InsertarFormulario(string correoElectronico, int identificacionEstudiante, string nombre, string apellido, int carreraID, bool cursosCompletos, int opcionGraduacionID)
+        {
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["ConexionBD"].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand("InsertarFormulario", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@CorreoElectronico", correoElectronico);
+                    command.Parameters.AddWithValue("@Identificacion", identificacionEstudiante);
+                    command.Parameters.AddWithValue("@Nombre", nombre);
+                    command.Parameters.AddWithValue("@Apellido", apellido);
+                    command.Parameters.AddWithValue("@CarreraID", carreraID);
+                    command.Parameters.AddWithValue("@CursosCompletos", cursosCompletos);
+                    command.Parameters.AddWithValue("@OpcionGraduacionID", opcionGraduacionID);
+
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
